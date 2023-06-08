b4b93e5ee00fbfb9f9a817a77d3fefa577903722
estebanjr.dev <tuapkmovil@gmail.com>
2023-06-08 15:28:07 -0500

    change color palette

--- app/src/main/java/com/ejrm/addme/ui/view/MainActivity.kt
+++ app/src/main/java/com/ejrm/addme/ui/view/MainActivity.kt
@@ -54,6 +54,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var addContactBinding: AddContactBinding
     lateinit var viewModel: MainViewModel
+    private lateinit var menu: Menu
     var dialog: AlertDialog? = null
     private lateinit var adapter: ContactAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -143,26 +144,6 @@ class MainActivity : AppCompatActivity() {
             connectivityManager?.activeNetworkInfo?.isConnected ?: false
         }
     }
-
-    /* fun checkForInternet(context: Context): Boolean {
-         val connectivityManager =
-             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-             val network = connectivityManager.activeNetwork ?: return false
-             val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-
-             return when {
-                 activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                 activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                 else -> false
-             }
-         } else {
-             @Suppress("DEPRECATION") val networkInfo =
-                 connectivityManager.activeNetworkInfo ?: return false
-             @Suppress("DEPRECATION")
-             return networkInfo.isConnected
-         }
-     }*/
     @RequiresPermission(anyOf = [Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET])
     suspend fun dataConexion(url: String): Boolean = try {
         withContext(Dispatchers.IO) {
@@ -180,27 +161,6 @@ class MainActivity : AppCompatActivity() {
         println("Error al conectar: ${e.message}")
         false
     }
-
-    /*  @RequiresPermission(anyOf = [Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.INTERNET])
-      suspend fun dataConexion(url: String): Boolean {
-          delay(1500)
-          val httpConnection: HttpURLConnection =
-              URL(url)
-                  .openConnection() as HttpURLConnection
-          if (checkForInternet(this)) {
-              try {
-                  httpConnection.setRequestProperty("User-Agent", "Android")
-                  httpConnection.connectTimeout = 1500
-                  httpConnection.connect()
-                  return httpConnection.responseCode == 200
-              } catch (e: Exception) {
-                  println(e.toString())
-                  return false
-              }
-          }
-          return false
-      }*/
-
     override fun onResume() {
         super.onResume()
         registerReceiver(estadoRed, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
@@ -219,10 +179,12 @@ class MainActivity : AppCompatActivity() {
                             Snackbar.LENGTH_INDEFINITE
                         ).show()
                         binding.viewLoading.isVisible = true
+                        menu.findItem(R.id.add)?.isVisible = false
                         binding.searchView.isVisible = false
                         binding.recyclerContact.isVisible = false
                     } else {
                         binding.viewLoading.isVisible = false
+                        menu.findItem(R.id.add)?.isVisible = true
                         binding.searchView.isVisible = true
                         binding.recyclerContact.isVisible = true
                         Snackbar.make(binding.root, "Conectado!!!", Snackbar.LENGTH_LONG).show()
@@ -236,6 +198,7 @@ class MainActivity : AppCompatActivity() {
                 )
                 snackbar.show()
                 binding.viewLoading.isVisible = true
+                menu.findItem(R.id.add)?.isVisible = false
                 binding.searchView.isVisible = false
                 binding.recyclerContact.isVisible = false
             }
@@ -244,6 +207,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
+        this.menu = menu!!
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -269,9 +233,6 @@ class MainActivity : AppCompatActivity() {
                         if (addContactBinding.CodeCountry.isValidFullNumber) {
                             addContactBinding.progress.isVisible = true
                             val phoneNumber = addContactBinding.CodeCountry.fullNumber.toString()
-                            val country = addContactBinding.CodeCountry.selectedCountryCode
-                            Snackbar.make(binding.root, phoneNumber, Snackbar.LENGTH_LONG)
-                                .show()
                             viewModel.loginViewModel(
                                 phoneNumber,
                                 addContactBinding.password.text.toString()
@@ -290,6 +251,10 @@ class MainActivity : AppCompatActivity() {
                             )
                                 .show()
                             dialog!!.dismiss()
+                            if (response.success){
+                                menu.findItem(R.id.face)?.isVisible = true
+                                menu.findItem(R.id.add)?.isVisible = false
+                            }
                         })
                         viewModel.getSuccessfulObserver()
                         viewModel.getAllContact()
@@ -347,89 +312,4 @@ class MainActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-
-    /*fun getStringImage(bitmap: Bitmap): String {
-        baos = ByteArrayOutputStream()
-       bitmap.compress(Bitmap.CompressFormat.JPEG,100,baos)
-        val array: ByteArray = baos.toByteArray()
-        return Base64.encodeToString(array,Base64.DEFAULT)
-    }
-
-    private fun checkPermissionAndOpenImageSelector() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            openImageSelector()
-        } else {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                    PERMISSION_CODE
-                )
-            }
-        }
-    }
-
-    private fun openImageSelector() {
-        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-        pickImage.launch(intent)
-    }
-
-    private val pickImage =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                result.data?.data?.let { uri ->
-                    imageUri = uri
-                    val foto = MediaStore.Images.Media.getBitmap(contentResolver, uri)
-                    bitmap  = Bitmap.createScaledBitmap(foto, 150, 150, true)
-                    addContactBinding.imgPerfil.setImageBitmap(bitmap)
-                }
-            }
-        }*/
-
-    fun getRequestBodyFromUri(uri: Uri): RequestBody {
-        val inputStream = this.contentResolver.openInputStream(uri)
-        val bytes = inputStream?.readBytes()
-        val MEDIA_TYPE_JPEG = "image/jpeg".toMediaTypeOrNull()
-        return RequestBody.create(MEDIA_TYPE_JPEG, bytes!!)
-    }
-
-    /* @RequiresApi(Build.VERSION_CODES.O)
-     fun upLoadMultiPart(name: String, phone: String, facebook:String, instagram: String){
-        /* val textName = RequestBody.create("text/plain".toMediaTypeOrNull(), name)
-         val textWhatsApp = RequestBody.create("text/plain".toMediaTypeOrNull(), phone)
-         val textFacebook = RequestBody.create("text/plain".toMediaTypeOrNull(), facebook)
-         val textInstagram = RequestBody.create("text/plain".toMediaTypeOrNull(), instagram)*/
-         val image = getRequestBodyFromUri(uri)
-         val currentDateTime = LocalDateTime.now()
-         val multipartFile = MultipartBody.Part.createFormData("image", name+"_"+currentDateTime.toString()+".png", image)
-         val requestBody = MultipartBody.Builder()
-             .setType(MultipartBody.FORM)
-             .addPart(multipartFile)
-             .addFormDataPart("name",name)
-             .addFormDataPart("phone",phone)
-             .addFormDataPart("facebook",facebook)
-             .addFormDataPart("instagram",instagram)
-             .build()
-         viewModel.add(name,phone,facebook,instagram)
-     }*/
-
-    /* override fun onRequestPermissionsResult(
-         requestCode: Int,
-         permissions: Array<out String>,
-         grantResults: IntArray
-     ) {
-         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-         if (requestCode == PERMISSION_CODE && grantResults.isNotEmpty()) {
-             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                 openImageSelector()
-             }
-         }
-     }
-
-     companion object {
-         private const val PERMISSION_CODE = 100
-     }*/
 }
